9133796c56a4b973dbe5cb795a8bb34a7a67bb82
deleuze199 <35817671+deleuze199@users.noreply.github.com>
2018-10-11 10:02:55 -0400

    Update Integration.java

--- Integration Project/src/Integration.java
+++ Integration Project/src/Integration.java
@@ -12,31 +12,24 @@ public class Integration {
     // assign scanner object
     scan = new Scanner(System.in);
     System.out.println("This project is to show what I have learned in COP 2006");
-
     String myName = "Benjamin"; // String with variable myName = "Benjamin"
     System.out.println("My name is " + myName);
+    
     String ageQuestion = "How old am I?"; // String with variable ageQuestion = "How old am I?"
     System.out.println(ageQuestion); // Print ageQuestion
-    double myAge = 19.0;; // double with variable name myAge have a value of 19
+    int myAge = 19;; // double with variable name myAge have a value of 19
     System.out.println("I am " + myAge); // Print "I am" and myAge
-    String bdayQuestion = "What day and month was I born?"; // String with variable bdayQuestion =
-                                                            // "when was I born"
+    String bdayQuestion = "What day and month was I born?"; // String with variable bdayQuestion
     System.out.println(bdayQuestion); // Print bdayQuestion
     final double myBday = 2.25; // double with variable name myBday have a value of 2.25
     System.out.println("I was born on " + myBday); // Print "I was born on " + myBday
 
-    // Ask what their name is
-    System.out.println("What is your first name?");
-    // Get user input
-    String yourName = scan.nextLine();
-    // use .trim() to get ride of spaces at the end of input
-    yourName = yourName.trim();
-    // use .length to get how many characters in name
-    int yourNameLength = yourName.length();
-    // use boolean and .equalsIgnoreCase to test if it was the same as my name
-    final boolean sameName = yourName.equalsIgnoreCase(myName);
-    // use boolean to test if length is grater than 9
-    final boolean length = yourNameLength > 9;
+    System.out.println("What is your first name?");// Ask what their name is
+    String yourName = scan.nextLine();// Get user input
+    yourName = yourName.trim();  // use .trim() to get ride of spaces at the end of input
+    int yourNameLength = yourName.length(); // use .length to get how many characters in name
+    final boolean sameName = yourName.equalsIgnoreCase(myName); // use boolean and .equalsIgnoreCase to test if it was the same as my name
+    final boolean length = yourNameLength > 9; // use boolean to test if length is grater than 9
     // if same name print line
     if (sameName) {
       System.out.println("We have the same name!");
@@ -52,28 +45,27 @@ public class Integration {
           + (yourName.length() == 1 ? "letter" : "letters") + " in your name.\nNot too long.");
     }
 
-    yourAge(myAge); // call method "yourAge" with argument myAge"
-    pickColor();
+    yourAge(myAge); // call method "yourAge" with argument "myAge"
+    pickColor(); // call method "pickColor" with no argument
 
-    // Output prompt
-    System.out
-        .println("Try to guess what number I am thin thinking of 0-10. Enter the number bellow.");
-    // Waiting for user to enter number
+    // Output prompt asking for int input
+    System.out.println("Try to guess what number I am thin thinking of 0-10. Enter the number"
+        + " below.");
+    // scan user int input
     int value = scan.nextInt(); // For a string input: String line = input.nextLine();
-
-    // close scanner
-    scan.close();
 
     getRandomNumberAndInput(value);
 
+    // close scanner
+    scan.close();
 
   }
 
   public static void yourAge(double myAge) { // method "yourAge" header with "double myAge"
                                              // parameters
-    Scanner ScanAge = new Scanner(System.in);
+  //  Scanner ScanAge = new Scanner(System.in);
     System.out.println("What is your age?");
-    int userAge = ScanAge.nextInt();
+    int userAge = scan.nextInt();
     int differenceInAge = userAge - (int) myAge; // casting double myAge to an int so the primitive
                                                  // data types match
     if (differenceInAge == 0) {
@@ -111,11 +103,9 @@ public class Integration {
 
   public static void pickColor() {
     System.out.println("What is my favorite color: Red, Blue, or Green");
-    String colorRed = "Red";
-    String colorBlue = "Blue";
-    String colorGreen = "Green";
-    Scanner ScanColor = new Scanner(System.in);
-    String userColor = ScanColor.nextLine();
+   // Scanner ScanColor = new Scanner(System.in);
+    scan.nextLine();
+    String userColor = scan.nextLine();
     switch (userColor) {
       case "Red":
         System.out.println("Not quiet, My favorite color is Green.");
