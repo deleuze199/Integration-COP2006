c947a280f33bb6dc0bb992a175fca91b02174d69
benjamindeleuze <benjamindeleuze@benjamins-mbp.attlocal.net>
2018-11-02 22:05:33 -0400

    Commented in why inheritance is beneficial

--- Integration Project/src/Cat.java
+++ Integration Project/src/Cat.java
@@ -1,4 +1,4 @@
-
+//Cat extends (inherits) Animal which is beneficial because Cat is an animal so we don't have rewrite all of the fields and methods in the Cat class
 public class Cat extends Animal {
     public Cat() {
         this.sound = "Meow";
--- Integration Project/src/Dog.java
+++ Integration Project/src/Dog.java
@@ -1,4 +1,4 @@
-
+//Dog extends (inherits) Animal which means it receives all of the fields and methods of the Animal Class
 public class Dog extends Animal {
     public Dog() {
         this.sound = "Woof";
